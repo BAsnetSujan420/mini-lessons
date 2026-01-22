d2dda303f271e76298d067087153b20f5bbac603
Sujan Basnet <basnetsujan1828420@gmail.com>
2026-01-22 16:48:43 +0545

    initial setup

---
+++ src/components/Card.jsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+function Card() {
+  return (
+    <Wrapper>
+      <p>
+        Otters have long, slim bodies and relatively short limbs. Their most
+        striking anatomical features are the powerful webbed feet used to swim,
+        and their seal-like abilities holding breath underwater.
+      </p>
+      <img
+        alt="A cute otter in water"
+        src="https://courses.joshwcomeau.com/cfj-mats/otter.jpg"
+      />
+
+      <p>
+        More importantly, otters are glorious water dogs, playful and curious.
+        The otter, no other, is the best animal.
+      </p>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  background-color: white;
+  padding: 32px;
+  color: black;
+  border-radius: 8px;
+  margin: 32px;
+  max-width: 500px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+export default Card;
