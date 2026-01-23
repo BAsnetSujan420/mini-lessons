6de1f20c242c1a3ec475914dfbc538e0c9233c65
Sujan Basnet <basnetsujan1828420@gmail.com>
2026-01-23 14:14:05 +0545

    three: bubble border

--- src/components/BorderedCard.jsx
+++ src/components/BorderedCard.jsx
@@ -16,10 +16,33 @@ const Box = styled.div`
   width: 400px;
   margin: 64px;
   border: 4px solid palevioletred;
+  position: relative;
 `;
 
-const MediumCircle = styled.div``;
-const BigCircle = styled.div``;
-const SmallCircle = styled.div``;
+const Circle = styled.div`
+  position: absolute;
+  border: inherit;
+  border-radius: 50%;
+  background-color: #242424;
+`;
+
+const MediumCircle = styled(Circle)`
+  width: 35px;
+  height: 35px;
+  left: -20px;
+  top: 35px;
+`;
+const BigCircle = styled(Circle)`
+  width: 65px;
+  height: 65px;
+  left: -20px;
+  top: -30px;
+`;
+const SmallCircle = styled(Circle)`
+  width: 25px;
+  height: 25px;
+  left: 55px;
+  top: -14px;
+`;
 
 export default BorderedCard;
